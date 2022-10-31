edd25ebe1e7b72a9f66134a0e34673fa194f9d43
Muhammadalifrontend <mhamzayev404@gmail.com>
2022-10-31 22:47:04 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,178 @@
+// M-1
+// let arr = [];
+// let t=prompt("son kiriting")
+// for (var i=0; i<t; i++) {
+//     arr.push(Math.round(Math.random()*10))
+// }
+// console.log(arr);
+// M-2
+// let n=prompt("n sonini kiriting:");
+// let arr=[];
+// for(i=1; i<=n; i++){
+//     arr.push(2**i);
+
+
+// }
+// console.log(arr);
+// M-3
+// let arr = [];
+// let sum=0;
+// let t=prompt("son kiriting")
+// for (var i=0; i<t; i++) {
+//     arr.push(Math.round(Math.random()*10))
+//     sum+=arr[i];
+// }
+// console.log(arr);
+// console.log(sum);
+// M-4
+// let arr=[];
+// let sum=0;
+// let ayirma=0;
+
+//     for(i=0; i<10; i++){
+//         arr.push(i*10)
+//         if(i%2==0){
+//             sum+=arr[i];
+//         }
+//         else if(i%2==1){
+//             ayirma-=arr[i];
+//         }
+//     }
+
+// console.log(arr);
+// console.log(sum);
+// console.log(ayirma);
+// M-6
+// let arr = [];
+// let t=prompt("son kiriting")
+// for(let a=t; a>0; a--){
+//     arr.push(a)
+// }
+// console.log(arr);
+// M-7
+// let a=[
+//     {ism:"ali",
+//     yosh:22},
+//     {ism:"vali",
+//     yosh:25},
+//     {ism:"g'ani",
+//     yosh:27}
+// ]
+// console.log(a[2].yosh-a[1].yosh);
+// console.log(a[0].yosh-a[1].yosh);
+// console.log(a[2].yosh-a[0].yosh);
+// let Array=[Math.floor(Math.random(12*100))]
+// for(let i=0; i<10; i++){
+//     if(i%2==0){
+//         console.log(Array);
+
+//     }
+// }
+// M-11
+// mas=[];
+// let a,b=0;
+// for(i=0; i<10; i++){
+//     mas.push(i)
+//     if(i%2==0){
+//         b=Math.max(i)
+//     }
+// }
+// console.log(mas);
+// console.log(b);
+
+// M-12
+// let a=0;
+// let arr = [];
+// let l=0;
+
+// let t=prompt("son kiriting")
+// for (var i=0; i<t; i++) {
+//     arr.push(Math.round(Math.random()*10))
+//    l=(a+=arr[i])/t;
+// }
+// console.log(arr);
+// console.log(l);
+// M-13
+// let mas=[4,5,8,6,77,88,6];
+// for(i=0; i<mas.length; i++){
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+
+
+// M-14
+// let mas=[];
+// let a,b=0;
+// for(i=0; i<10; i++){
+//     mas.push(i);
+
+//     // console.log(mas);
+//     for(k=3; k<10; k++){
+//         if(i%k==0){
+//             b++
+//             // console.log(b);
+//             if(b>2){
+//                 console.log(mas);
+//             }
+//         }
+//     }
+// }
+    // console.log(mas[i]);
+// M-15
+// let n = 30;
+// let arr = []
+// for (let i = 1; i <= n; i++) {
+//     arr.push(i)
+// }
+// console.log(arr)
+// for (let j = 0; j < n; j++) {
+//     if(arr[j]%2==1){
+//         console.log(arr[j])
+//     }
+// }
+// console.log(arr);
+// 16-M
+// let mas=[2,true, "salom",null, undefined, 'Muhammadali'];
+// for(let i=0; i<mas.length; i++){
+//    console.log(mas[i]!=isNaN);
+// }
+
+
+// M-17
+// mas1=[2,4,32,67];
+// mas2=[2,4,82,61];
+// mas3=[];
+// for(let i=0; i<=mas1.length; i++){
+//     mas1.push(i)
+// }
+// for(let j=0; j<=mas2.length; j++){
+//     mas2.push(j)
+// } 
+// console.log(mas1);
+// console.log(mas2);
+// 18-M
+// let mas=[];
+// let n=prompt("n=")
+// for(i=0; i<n; i++){
+//     let m=prompt("m ga son kiriting ");
+//     mas.push(m);
+// }
+// console.log(mas);
+// M-19
+// let mas=[];
+// let sum=0;
+// let n=prompt("n=")
+// for(i=0; i<n; i++){
+//     let m=+prompt("m ga son kiriting ");
+//     sum+=m;
+//     mas.push(m);
+// }
+// console.log(sum);
+// console.log(mas);
+// M-20
+// M-21
+let mas=[4,8,6,5,4,9];
+console.log(mas[-1]);
+
+
